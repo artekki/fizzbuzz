046360e8ca82c81bc1a929a600e891ec5534d5b7
artekki <djrpy4all@gmail.com>
2015-08-27 11:07:21 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+var x = prompt("type number");
+
+function fizzbuzz (number) {
+	 
+	if (number % 3 == 0 && number % 5 == 0) {
+		return "fizzbuzz"
+	}	
+
+	else if (number % 5 == 0) {
+		return "fizz"
+	}
+
+	else if (number % 3 == 0) {
+		return "buzz"
+	}
+
+	else {
+		return number 
+	};
+
+}
+
+var returnHTML = '';
+
+for(var i =1; i < x; i++) {
+
+	returnHTML = returnHTML + fizzbuzz(i) + "<br>";
+	
+}
+
+$("#displayfb").append(returnHTML)
+  
+})
+
+
+
